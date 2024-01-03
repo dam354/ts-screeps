00e03a056e280b6e199a7bbb7afce65ecc2714bf
Dan Mennella <dmennella34@gmail.com>
2024-01-02 23:58:12 -0500

    Add harvester and upgrader roles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { ErrorMapper } from "utils/ErrorMapper";
+import { roleHarvester } from "roles/role.harvester";
+import { roleUpgrader } from "roles/role.upgrader";
 
 declare global {
   /*
@@ -20,13 +22,17 @@ declare global {
     room: string;
     working: boolean;
   }
-
+  interface Creep {
+    findEnergySource(): Source | null;
+  }
   // Syntax for adding proprties to `global` (ex "global.log")
   namespace NodeJS {
     interface Global {
       log: any;
     }
   }
+
+
 }
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
@@ -34,6 +40,34 @@ declare global {
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  console.log('Harvesters: ' + harvesters.length);
+
+  if (harvesters.length < 2) {
+    var newName = 'Harvester' + Game.time;
+    console.log('Spawning new harvester: ' + newName);
+    Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
+      { memory: { role: 'harvester', room: '', working: false } });
+  }
+
+  if (Game.spawns['Spawn1'].spawning) {
+    var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+    Game.spawns['Spawn1'].room.visual.text(
+      '🛠️' + spawningCreep.memory.role,
+      Game.spawns['Spawn1'].pos.x + 1,
+      Game.spawns['Spawn1'].pos.y,
+      { align: 'left', opacity: 0.8 });
+  }
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester') {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+  }
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
